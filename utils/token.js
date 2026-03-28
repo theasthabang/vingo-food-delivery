9316a36347941f83e201801f383d65ff42755ba4
theasthabang <bangastha30@gmail.com>
2026-03-28 09:37:38 +0530

    fix: smtp port 587

--- utils/token.js
+++ utils/token.js
@@ -1,12 +1,13 @@
 import jwt from "jsonwebtoken"
 
-const genToken = async (userId) => {
-  try{
-    const token = await jwt.sign({userId},process.env.JWT_SECRET , {expiresIn:"4d"})
-    return token
-  }catch(error){
-    console.log(error)
-
-  }
+const genToken = (userId) => {
+  if (!userId) throw new Error("userId is required to generate token")
+  
+  return jwt.sign(
+    { userId },
+    process.env.JWT_SECRET,
+    { expiresIn: "4d" }
+  )
 }
- export default genToken
+
+export default genToken
